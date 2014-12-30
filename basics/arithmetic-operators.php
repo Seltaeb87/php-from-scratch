ad547b1513e666b8df993c8d61b1be881b68a164
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-12-30 22:01:13 +0100

    Arithmetic Operators

---
+++ basics/arithmetic-operators.php
@@ -0,0 +1,36 @@
+<?php
+
+// arithmetic operators
+$iNum = 10;
+$iNum2 = 3;
+
+echo 'Variable iNum: ' . $iNum . '<br>';
+echo 'Variable iNum2: ' . $iNum2 . '<br><br>';
+
+//the plus +
+$result = $iNum + $iNum2;
+echo 'Plus result: ' . $result . '<br>';
+
+//the minus -
+$result = $iNum - $iNum2;
+echo 'Minus result: ' . $result . '<br>';
+
+//the divided /
+$result = $iNum / $iNum2;
+echo 'Divided result: ' . $result . '<br>';
+
+//the multiply *
+$result = $iNum * $iNum2;
+echo 'Multiplied result: ' . $result . '<br>';
+
+//the modulus %
+$result = $iNum % $iNum2;
+echo 'Multiplied result: ' . $result . '<br>';
+
+// Modulus in this case: 3x3=9 and the leftover is 1. That's the modulus.
+
+
+
+
+
+?>
