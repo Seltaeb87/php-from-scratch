d47fb5af8e0975b6845b39a22907d347e884a1d2
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-12-30 21:20:05 +0100

    Added the Basics section folder

---
+++ basics/variables.php
@@ -0,0 +1,10 @@
+<?php
+
+//Variables
+
+$variable = 'hi';
+$sName = 'Bastiaan'; //the 's' stands for String.
+
+//a variable can have all sorts of data: Strings, numbers, booleans(true/false) and so on.
+
+?>
