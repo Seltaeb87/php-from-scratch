87e9a5f7431f97e9b45a3e5b6db5d4b121d5bfaf
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-12-30 21:35:53 +0100

    If statement with age example is added

--- basics/if-statements.php
+++ basics/if-statements.php
@@ -2,13 +2,20 @@
 
 // create variable with a name. In this case: Bastiaan
 $sName = 'Bastiaan';
+$iAge  = 27;
 
 // If $sName is equal to Bastiaan(True), than that's my name. Else(false): That's not my name.
 	if ($sName == 'Bastiaan') {
-			echo 'That\'s right, your name is Bastiaan';
+			echo 'That\'s right, your name is Bastiaan <br>';
 		}else{
-			echo 'Your name is not Bastiaan!';
+			echo 'Your name is not Bastiaan! <br>';
 		}	
+
+		if ($iAge <= 27) {
+			echo 'Wow, you\'re a young dude!';
+		}else{
+			echo 'Damn, you\'re old!';
+		}
 
 
 ?>
