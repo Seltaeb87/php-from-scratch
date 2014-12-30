bc0cada84d2d3c7a5186b76e4c72bf5634fd2df5
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-12-30 21:29:03 +0100

    Added comments

--- basics/if-statements.php
+++ basics/if-statements.php
@@ -1,7 +1,9 @@
 <?php
 
+// create variable with a name. In this case: Bastiaan
 $sName = 'Bastiaan';
 
+// If $sName is equal to Bastiaan(True), than that's my name. Else(false): That's not my name.
 	if ($sName == 'Bastiaan') {
 			echo 'That\'s right, your name is Bastiaan';
 		}else{
