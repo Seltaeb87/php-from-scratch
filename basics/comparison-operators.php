d3d0e49374050f812b30eb3c9ca59d23e5c4958a
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-12-31 09:58:50 +0100

    Comparison Operators

---
+++ basics/comparison-operators.php
@@ -0,0 +1,24 @@
+<?php
+
+//comparison operators: == <= => !=
+
+	$iNum = 27;
+
+	//if variable $iNum is equal to 27, then proceed. If it's not equal, do nothing(no output).
+	if ($iNum == 27) {
+		echo 'The number 27 is true. <br>';
+	}
+
+	//if $iNum is less then or equal to 23, you must be 23 or younger. Else you must be older then 23.
+	if ($iNum <= 23) {
+		echo 'You\'re 23 or younger';
+	}else{
+		echo 'You\'re older than 23';
+	}
+
+
+
+
+
+
+?>
