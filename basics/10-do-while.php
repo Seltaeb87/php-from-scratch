6fa636dd8081b5a12e6bd6505d9387acbdc67e6e
Seltaeb87 <bastiaanbitter87@gmail.com>
2015-01-05 09:39:03 +0100

    Do-While Loop

---
+++ basics/10-do-while.php
@@ -0,0 +1,16 @@
+<?php
+
+	//Do-While loops
+	
+	$iNum = 1;
+
+	//Do output $iNum and do a +1 each time it loops. Stop the loop when it reaches 10 ('While ($iNum <= 10)').
+	// The advantage is that the 'Do' is always executed until it reaches the given code in While.
+	do {
+		echo $iNum . '<br>';
+		$iNum++;
+	} while ( $iNum <= 10);
+
+
+
+?>
