a69bff970767134be47c5f07626b35f174f4ebdf
Seltaeb87 <bastiaanbitter87@gmail.com>
2015-01-05 09:48:26 +0100

    For-Loops

---
+++ basics/11-for-loops.php
@@ -0,0 +1,16 @@
+<?php
+
+	//For loops
+
+	/*
+	  for $i is 0, $i is less or equal to 10, $i + 1. Loop until you reach the max of 10. Output the result everytime it loops.
+	  So start at 0. Is it less then or equal to 10? 
+     		Yes: Proceed wit a +1. Continues to do this until it reach the given max. In this case, 10.
+     		No: Stop the loop.
+    */ 
+
+	for ($i=0; $i <= 10 ; $i++) { 
+		echo $i . '<br>';
+	}
+
+?>
