da349b69171186778bc3c6864edca8bbb69c97e6
Seltaeb87 <bastiaanbitter87@gmail.com>
2015-01-15 17:46:25 +0100

    Added additional comments

--- basics/16-GET.php
+++ basics/16-GET.php
@@ -1,11 +1,10 @@
-<form action="16-GET.php" method="GET">
-	Name: <br><input type="text" name ="name"><br>
-	Age: <br><input type="text" name ="age"><br>
-		<inpyt type="hidden" name ="submitted" value="true">
-		<input type = "submit" value="Submit">
+	<form action="16-GET.php" method="GET">
+		Name: <br><input type="text" name ="name"><br>
+		Age: <br><input type="text" name ="age"><br>
+			<inpyt type="hidden" name ="submitted" value="true">
+			<input type = "submit" value="Submit">
 
-</form>
-
+	</form>
 
 
 <?php
@@ -15,11 +14,12 @@
 	$sName = $_GET['name'];
 	$iAge = $_GET['age'];
 
+	// Check if name and age are set in the form
 	if (isset($sName) && isset($iAge)) {
-		if (!empty($sName) && !empty($iAge)) {
-			echo 'I am ' . $sName . ' and I am ' . $iAge . ' years old.';	
+		if (!empty($sName) && !empty($iAge)) {    //Check if 'name' and 'age' formfields are not emptry
+			echo 'I am ' . $sName . ' and I am ' . $iAge . ' years old.'; // If everything is correct, this will be the output
 		}else{
-			echo 'Fill in the form.';
+			echo 'Fill in the form.'; // If a field is empty, show this output.
 		}
 
 	}
