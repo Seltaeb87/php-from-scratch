6e56eef55ff58c5335b0158f3e8c918281011476
Seltaeb87 <bastiaanbitter87@gmail.com>
2015-03-03 22:23:20 +0100

    Cookies, part one

---
+++ basics/22-cookies-part-one.php
@@ -0,0 +1,15 @@
+<?php
+//Cookies
+
+/*
+$exp = time()+86400;
+setcookie("name", "Bastiaan", $exp);
+*/
+
+//setcookie("age", 26, $exp);
+
+echo $_COOKIE['name'] . " is " . $_COOKIE['age'];
+
+
+
+?>
